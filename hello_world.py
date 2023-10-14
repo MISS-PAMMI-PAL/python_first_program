2a12720685e1e64c6af79b5a347cb1437ae478e9
MISS-PAMMI-PAL <pammipal24@gmail.com>
2023-10-14 14:42:50 +0530

    add project HelloWorld

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+# Project : Print Hello World
+# Author  : Pammi Pal
+# Created : 10-14-2023
+# Updated : 10-14-2023
+
+import os
+
+def print_text(text):
+    print("Hello world !")
+    print(" Your Text is ", text)
+
+if __name__ == "__main__":
+    print_text("Pammi Pal")
